564c87b5710acfa37206a1bc7876ca3d73ce4b33
Majid Asgari <majid.asgari@gmail.com>
2017-04-15 21:15:53 +0430

    unit test

--- connector/src/main/java/ir/ac/iust/dml/kg/virtuoso/connector/VirtuosoConnector.java
+++ connector/src/main/java/ir/ac/iust/dml/kg/virtuoso/connector/VirtuosoConnector.java
@@ -1,5 +1,7 @@
 package ir.ac.iust.dml.kg.virtuoso.connector;
 
+import com.hp.hpl.jena.graph.Node;
+import com.hp.hpl.jena.graph.Triple;
 import com.hp.hpl.jena.query.*;
 import com.hp.hpl.jena.rdf.model.*;
 import ir.ac.iust.dml.kg.raw.utils.ConfigReader;
@@ -14,23 +16,46 @@ import java.util.List;
 public class VirtuosoConnector {
 
   final private VirtGraph graph;
+  final private String graphName;
   final private Model model;
 
+  public VirtuosoConnector() {
+    this("http://fkg.iust.ac.ir/");
+  }
+
   public VirtuosoConnector(String graphName) {
-    final String virtuosoServer = ConfigReader.INSTANCE.getString("virtuoso.address",
-            "localhost:1111");
-    final String virtuosoUser = ConfigReader.INSTANCE.getString("virtuoso.user", "dba");
-    final String virtuosoPass = ConfigReader.INSTANCE.getString("virtuoso.password", "admin");
-    graph = new VirtGraph(graphName, "jdbc:virtuoso://" + virtuosoServer, virtuosoUser, virtuosoPass);
-    model = ModelFactory.createModelForGraph(graph);
+    this(graphName, ConfigReader.INSTANCE.getString("virtuoso.address",
+            "localhost:1111"),
+            ConfigReader.INSTANCE.getString("virtuoso.user", "dba"),
+            ConfigReader.INSTANCE.getString("virtuoso.password", "admin"));
   }
 
   public VirtuosoConnector(String graphName, String serverAddress, String username, String password) {
+    this.graphName = graphName;
     graph = new VirtGraph(graphName, "jdbc:virtuoso://" + serverAddress, username, password);
     model = ModelFactory.createModelForGraph(graph);
   }
 
+  public void clear() {
+    graph.clear();
+  }
+
+  public void addResource(String subject, String predicate, String object) {
+    final Node s = Node.createURI(subject.contains("://") ? subject : graphName + subject);
+    final Node p = Node.createURI(predicate.contains("://") ? predicate : graphName + predicate);
+    final Node o = Node.createURI(object.contains("://") ? object : graphName + object);
+    graph.add(new Triple(s, p, o));
+  }
+
+  public void addLiteral(String subject, String predicate, Object object) {
+    final Node s = Node.createURI(subject.contains("://") ? subject : graphName + subject);
+    final Node p = Node.createURI(predicate.contains("://") ? predicate : graphName + predicate);
+    final Literal o = ResourceFactory.createTypedLiteral(object);
+    graph.add(new Triple(s, p, o.asNode()));
+  }
+
   public List<VirtuosoTriple> getTriplesOfSubject(String subject) {
+    subject = subject.contains("://") ? subject : graphName + subject;
     String queryString =
             "SELECT ?p ?o\n" +
                     "WHERE {\n" +
@@ -51,6 +76,8 @@ public class VirtuosoConnector {
   }
 
   public List<VirtuosoTriple> getTriples(String subject, String predicate) {
+    subject = subject.contains("://") ? subject : graphName + subject;
+    predicate = predicate.contains("://") ? predicate : graphName + predicate;
     String queryString =
             "SELECT ?p ?o\n" +
                     "WHERE {\n" +
@@ -85,11 +112,21 @@ public class VirtuosoConnector {
     else if (o instanceof Literal) {
       final Literal l = (Literal) o;
       final VirtuosoTripleType type;
-      if (l.getDatatypeURI().endsWith("long")) type = VirtuosoTripleType.Long;
-      else if (l.getDatatypeURI().endsWith("int")) type = VirtuosoTripleType.Int;
-      else if (l.getDatatypeURI().endsWith("double")) type = VirtuosoTripleType.Double;
-      else type = VirtuosoTripleType.String;
-      return new VirtuosoTripleObject(type, o.toString(), ((Literal) o).getLanguage());
+      final String value;
+      if (l.getDatatypeURI().endsWith("long")) {
+        type = VirtuosoTripleType.Long;
+        value = l.getLong() + "";
+      } else if (l.getDatatypeURI().endsWith("int")) {
+        type = VirtuosoTripleType.Int;
+        value = l.getInt() + "";
+      } else if (l.getDatatypeURI().endsWith("double")) {
+        type = VirtuosoTripleType.Double;
+        value = l.getDouble() + "";
+      } else {
+        type = VirtuosoTripleType.String;
+        value = l.getString();
+      }
+      return new VirtuosoTripleObject(type, value, l.getLanguage());
     }
     return null;
   }
---
+++ connector/src/test/java/ir/ac/iust/dml/kg/virtuoso/Tester.java
@@ -0,0 +1,39 @@
+package ir.ac.iust.dml.kg.virtuoso;
+
+import ir.ac.iust.dml.kg.virtuoso.connector.VirtuosoConnector;
+import ir.ac.iust.dml.kg.virtuoso.connector.data.VirtuosoTriple;
+import org.junit.Test;
+
+import java.util.List;
+
+public class Tester {
+  @Test
+  public void test() {
+    final String GRAPH_NAME = "http://fkg.iust.ac.ir/test/";
+
+    final VirtuosoConnector connector = new VirtuosoConnector(GRAPH_NAME);
+    connector.clear();
+    connector.addResource("s1", "p1", "o1");
+    connector.addLiteral("s1", "p2", "string");
+    connector.addLiteral("s1", "p1", 0.2);
+
+    List<VirtuosoTriple> triples = connector.getTriplesOfSubject("s1");
+    for (VirtuosoTriple t : triples) {
+      final String value = t.getObject().getValue();
+      switch (t.getObject().getType()) {
+        case Double:
+          assert value.equals("0.2");
+          break;
+        case Resource:
+          assert value.equals(GRAPH_NAME + "o1");
+          break;
+        default:
+          assert value.equals("string");
+      }
+    }
+
+    triples = connector.getTriples("s1", "p2");
+    assert triples.size() == 1;
+    assert triples.get(0).getObject().getValue().equals("string");
+  }
+}
